3897e09bd26c2c7c71837272efb4c5bc6f9ad343
UtkarshKharb <42812210+UtkarshKharb@users.noreply.github.com>
2020-10-02 21:13:48 +0530

    Add files via upload

---
+++ chessclock.js
@@ -0,0 +1,38 @@
+var distance = 10;
+var startButton = document.getElementById("start");
+var resetButton = document.getElementById("reset");
+
+var time1 = document.getElementById("time1").textContent = distance;
+var time2 = document.getElementById("time2").textContent = distance;
+var check = false;
+resetButton.addEventListener("click",function(){
+	location.reload();
+})
+startButton.addEventListener("click",function() {setInterval( function()
+{
+	if(document.getElementById("time1").textContent == 0)
+    {
+    	clearInterval();
+        alert("Player 1 Lost");
+        location.reload();
+    }
+    document.getElementById("time1").textContent--;
+
+    
+} 
+,1000 );});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
